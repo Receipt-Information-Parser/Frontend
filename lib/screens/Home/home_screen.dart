bb64d5240f305199249710a9b0e831a6844f2925
Charlieppark <chanho0309@gmail.com>
2023-05-27 20:57:49 +0900

    feat(receipt_list_screen): 영수증 추가 button 생성

--- lib/screens/Home/home_screen.dart
+++ lib/screens/Home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:rip_front/constants.dart';
+import 'package:rip_front/screens/Login/login_screen.dart';
 
 import '../../../http/dto.dart';
 import '../../../models/current_index.dart';
@@ -14,6 +15,58 @@ class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() {
     return _HomeScreenState();
+  }
+}
+
+class BottomSheetWidget extends StatefulWidget {
+  const BottomSheetWidget({super.key});
+
+  @override
+  _BottomSheetWidgetState createState() => _BottomSheetWidgetState();
+}
+
+
+class _BottomSheetWidgetState extends State<BottomSheetWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 175,
+        color: Color.fromRGBO(239, 243, 255, 100),
+        //specify height, so that it does not fill the entire screen
+        child: Column(
+            children: [
+              Expanded(child: Row()),
+              ListTile(
+                contentPadding: const EdgeInsets.only(left: 40),
+                minVerticalPadding: 20,
+                horizontalTitleGap: 20,
+                title: const Text(
+                    "영수증 촬영하기",
+                    style: TextStyle(
+                        fontSize: 23,
+                        color: Colors.black)
+                ),
+                leading: const Icon(Icons.camera_alt, size: 40,),
+                onTap: () {
+                },
+              ),
+              ListTile(
+                contentPadding: const EdgeInsets.only(left: 40),
+                minVerticalPadding: 20,
+                horizontalTitleGap: 20,
+                title: const Text(
+                    "갤러리에서 영수증 찾기",
+                    style: TextStyle(
+                        fontSize: 23,
+                        color: Colors.black)
+                ),
+                leading: const Icon(Icons.photo, size: 40),
+                onTap: () {
+                },
+              ),
+            ]
+        ) //what you want to have inside, I suggest using a column
+    );
   }
 }
 
@@ -53,14 +106,22 @@ class _HomeScreenState extends State<HomeScreen> {
     DateFormat('yyyy년 MM월 dd일 kk:mm').format(DateTime.now()),
   ];
 
+  final scaffoldState = GlobalKey<ScaffoldState>();
+  bool bottomSheetToggle = false;
+
   @override
   Widget build(BuildContext context) {
+
+    PersistentBottomSheetController? _controller;
+
     final CurrentIndex currentIndex = Provider.of<CurrentIndex>(context);
     UserId userId = Provider.of<UserId>(context);
 
     TokenResponse tokenResponse = Provider.of<TokenResponse>(context);
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      key: scaffoldState,
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(100.0),
         child: Container(
@@ -129,16 +190,35 @@ class _HomeScreenState extends State<HomeScreen> {
           ))
         ],
       ),
+      floatingActionButton: Container(
+        height: 70,
+        width: 70,//Floating action button on Scaffold
+        child: FittedBox(
+            child: FloatingActionButton(
+              onPressed: () {
+                if (bottomSheetToggle == false) {
+                  _controller = scaffoldState.currentState?.showBottomSheet((context) => const BottomSheetWidget());
+                  bottomSheetToggle = true;
+                } else {
+                  _controller?.close();
+                  bottomSheetToggle = false;
+                }
+              },
+              child: Icon(Icons.add, size: 40), //icon inside button
+      ))),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      //floating action button position to center
       bottomNavigationBar: BottomNavigationBar(
+        iconSize: 32,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.list), label: "영수증 목록"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "영수증 추가"),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "영수증 추가"),
           BottomNavigationBarItem(icon: Icon(Icons.settings), label: "설정"),
         ],
         currentIndex: currentIndex.index,
         selectedItemColor: defaultColor,
         onTap: ((value) {
-
           setState(() {
             currentIndex.setCurrentIndex(value);
             switch (currentIndex.index) {
