d3a92f0f3d800c1c001d4c5fafb5d9176cd47d0b
JWseop <tjqdnwj@gmail.com>
2023-06-02 22:42:17 +0900

    fix[myinfo_screen]:프로필 사진 변경시 서버에 savePicture API호출, 추가 디버깅 필요(백엔드 완료시 시작)

--- lib/http/request.dart
+++ lib/http/request.dart
@@ -4,13 +4,8 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'dto.dart';
-
-// 지금까지 구현된 것
-// 로그인, 회원가입, 공모전 생성, 사용자 설정
-
-// 구현해야 할 것
-// 공모전 목록 가져오기, 참가, 참가 승인
 
 Future<UserResponse> SignUp(String url, SignUpRequest signUpRequest) async {
   final response = await http.post(
@@ -43,28 +38,6 @@ Future<UserResponse> Login(String url, LoginRequest loginRequest) async {
     throw Exception('Failed to log in.');
   }
 }
-// for debug
-// Future<UserResponse> Login(String url, LoginRequest loginRequest) async {
-//   print("[debug]: Login Post Start, url:${url}");
-//   try {
-//     final response = await http.post(
-//       Uri.parse(url),
-//       headers: <String, String>{
-//         'Content-Type': 'application/json; charset=UTF-8',
-//       },
-//       body: loginRequest.toJson(),
-//     );
-//     print("[debug]: Login Post fin");
-//     if (response.statusCode == 200) {
-//       return UserResponse.fromJson(jsonDecode(response.body));
-//     } else {
-//       throw Exception('Failed to log in. Response status code: ${response.statusCode}');
-//     }
-//   } catch (e) {
-//     print('There was a problem with the login request: $e');
-//     throw e;
-//   }
-// }
 
 Future<MessageResponse> existsEmail(String url, EmailRequest emailRequest) async {
   final response = await http.post(
@@ -147,16 +120,7 @@ Future<MessageResponse> resetPassword(String url, EmailRequest emailRequest) asy
     throw Exception('Failed to reset password.');
   }
 }
-// Future<UserResponse> modifyNickname(
-//     String url, ModifyRequest modifyRequest, String? token) async {
-//   final response = await http.post(
-//     Uri.parse(url),
-//     headers: <String, String>{
-//       'Content-Type': 'application/json; charset=UTF-8',
-//       HttpHeaders.authorizationHeader: "Bearer ${token!}"
-//     },
-//     body: modifyRequest.toJson(),
-//   );
+
 Future<List<PictureResponse>> getPictureList(String url, String? token) async {
   final response = await http.get(
     Uri.parse(url),
@@ -173,23 +137,29 @@ Future<List<PictureResponse>> getPictureList(String url, String? token) async {
   }
 }
 
-Future<File> getPictureObject(String url,String object) async {
-  var request = await HttpClient().getUrl(Uri.parse('$url/$object'));
+Future getPictureObject(String url, String object, String? token) async {
+  var httpClient = HttpClient();
+  var request = await httpClient.getUrl(Uri.parse('$url/$object'));
+
+  // Bearer token
+  request.headers.set('authorization', 'Bearer $token');
+
   print('[debug]url:$url/$object');
   var response = await request.close();
   var bytes = await consolidateHttpClientResponseBytes(response);
-  String _dir = (await getApplicationDocumentsDirectory()).path;
-  var dir = Directory('$_dir/profile');
-  if (!await dir.exists()) {
-    await dir.create(recursive: true); // recursively create all non-existent directories
-  }
-  File file = File('$dir/profile.jpg');
+  final dir = await getExternalStorageDirectory();
+  File file = File('${dir!.path}/$object');
   await file.writeAsBytes(bytes);
   return file;
 }
 
-Future<PictureResponse> savePicture(String url,File file) async {
+
+Future<KeyResponse> savePicture(String url, File file, String? token) async {
   var request = http.MultipartRequest('POST', Uri.parse(url));
+
+  // Bearer token 추가
+  request.headers.addAll({'Authorization': 'Bearer $token'});
+
   request.files.add(http.MultipartFile('file', file.readAsBytes().asStream(), file.lengthSync(), filename: file.path.split("/").last));
 
   var response = await request.send();
@@ -197,7 +167,7 @@ Future<PictureResponse> savePicture(String url,File file) async {
   if (response.statusCode == 200) {
     var responseData = await response.stream.toBytes();
     var responseString = String.fromCharCodes(responseData);
-    return PictureResponse.fromJson(json.decode(responseString));
+    return KeyResponse.fromJson(json.decode(responseString));
   } else {
     throw Exception('Failed to save picture');
   }
